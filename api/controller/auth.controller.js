bd2bc5405d5d73c485e54f93ff7726e2c80d6156
Adrijchakraborty <adrijchakraborty8@gmail.com>
2024-04-06 17:13:11 +0530

    Backend completed

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -77,5 +77,12 @@ export const login = async (req, res) => {
 	}
 }
 export const logout = (req, res) => {
-    console.log("logout");
+    try {
+		res.cookie("jwt","" ,{maxage:0});
+		res.status(200).json({message: "Logout successfully"});
+		
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import  express  from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import messageRoutes from "./routes/message.route.js"
+import userRoutes from "./routes/user.route.js"
+
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -10,7 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());//to parse the incoming request with JSON payload(from req.body)
-app.use("/api/auth",authRoutes);
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 // app.get('/test', function(req, res) {
